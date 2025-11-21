5ff0d3f4b2a3dcb9d66c8e8b7398a1557ce95ef0
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2025-11-21 22:07:45 +0530

    solve the basic questions

---
+++ javaBasics/basic.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class javaBasics{
+    public static void main(String args[]){
+    
+int a =5;
+int b=10;
+
+String ram = "hii" ;
+
+System.out.println("****");
+System.out.println("***");
+System.out.print("***");
+System.out.println("hello ram");;
+System.out.println(a);
+System.out.println(ram);
+a=b;
+System.out.println(a);
+
+}
+}
