53535b47b431d30f75641405177d0d65c1b1ef4d
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-01-14 23:54:09 +0530

    add question 13

---
+++ Q12.java
@@ -0,0 +1,19 @@
+//print 10 times hello ram using do while loops?
+
+import java.util.*;
+public class JavaBasics{
+    public static void main(String args[]){
+
+int count = 1;
+
+do {
+
+    System.out.println("hello world");
+    count++;
+
+    
+} while (count<=10);
+
+}
+}
+
