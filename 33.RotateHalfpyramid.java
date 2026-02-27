4cbd580563288ef4622485fd41d1d17c4753c603
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-02-27 19:08:00 +0530

    Inverted half pyramid

---
+++ 33.RotateHalfpyramid.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class java{
+
+public static void HalfPyramid(int n){
+
+    for(int i=1; i<=n;i++){
+
+        for(int j=1;j<=n-1;j++){
+            System.out.print(" ");
+        }
+
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+
+public static void main(String args[]){
+    HalfPyramid(7);
+}
+}
