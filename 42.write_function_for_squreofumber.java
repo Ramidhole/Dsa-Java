58e1a39ba95207a696bfccfa46dcfa2bb1ba8443
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-03-12 21:54:58 +0530

    write a function for square  of number

---
+++ 42.write_function_for_squreofumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class dsaJava{
+    public static void SquareofNumber(int n){
+
+        int square = n*n;
+
+        System.out.println(square);
+    }
+
+    public static void main(String[] args) {
+        
+        SquareofNumber(7);
+    }
+}
