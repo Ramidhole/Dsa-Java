27f405fbd24a5e0b7c1a444b1736d9882b35127a
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-03-12 21:36:00 +0530

    count the given number

---
+++ 41.write_programme_count_how_many_digit_in_number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class JavaBasics{
+ public static void main(String args[]){
+
+   
+   int number = 23567;
+   int count = 0;
+   
+    while(number != 0){
+    number= number /10;
+
+    count++;
+
+    }
+
+    System.out.println(count);
+
+    }
+} 
+
+
