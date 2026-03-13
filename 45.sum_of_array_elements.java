a9aa1b47b0e72cfdff4c03e91b9902950bd32c3a
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-03-13 11:08:05 +0530

    sum of arrays elements

---
+++ 45.sum_of_array_elements.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String args[]){
+
+
+int arr[]= {4,3,4,1,2,2};
+int sum =0;
+
+for (int i = 0; i < arr.length; i++) {
+
+    sum+=arr[i];
+    
+}
+System.out.println(sum);
+}
+}
