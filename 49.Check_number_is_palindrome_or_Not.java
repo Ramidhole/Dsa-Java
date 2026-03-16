796984adce9eaa7a8e6a10e025cfa82394482d7e
Ramidhole <121009055+Ramidhole@users.noreply.github.com>
2026-03-16 20:46:43 +0530

    check the number is palindrome or not

---
+++ 49.Check_number_is_palindrome_or_Not.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Javadsa{
+    public static void main(String args[]){
+
+   Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+
+   int num = sc.nextInt();
+
+   int original = num;
+   int reverse = 0;
+
+   while(num > 0){
+
+    int lastDigit = num %10;
+
+reverse = reverse * 10 +lastDigit;
+
+num = num/10;
+   }
+
+if(original == reverse){
+    System.out.println("the number is palindrome");
+}
+else{
+    System.out.println("the number is not palindrome");
+}
+   }
+
+}
